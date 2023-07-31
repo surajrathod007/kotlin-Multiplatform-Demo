8866f460fb0053d40888decad58be5a3b75c0aa3
SurajSinh Rathod <surajsinhrathod75@gmail.com>
2023-07-31 13:27:44 +0530

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "kotlin_Multiplatform_Demo"
+include(":androidApp")
+include(":shared")
